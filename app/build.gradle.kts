6965a1e9d26ec02fd1704bfff48be82da4572a78
Ngenge Senior <seniorngenge@gmail.com>
2026-03-26 14:08:10 -0400

    Changed package name from com.proofmode.c2pa to org.contentauth.android.sample.c2pa (#5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,11 +8,11 @@ plugins {
 }
 
 android {
-    namespace = "com.proofmode.c2pa"
+    namespace = "org.contentauth.android.sample.c2pa"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.proofmode.c2pa"
+        applicationId = "org.contentauth.android.sample.c2pa"
         minSdk = 28
         targetSdk = 36
         versionCode = 1
--- app/src/main/java/com/proofmode/c2pa/data/Media.kt
+++
@@ -1,9 +0,0 @@
-package com.proofmode.c2pa.data
-
-import android.net.Uri
-
-data class Media(
-    val uri: Uri,
-    val isVideo: Boolean,
-    val date: Long,
-)
